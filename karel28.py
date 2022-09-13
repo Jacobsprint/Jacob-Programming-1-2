594045572496832c3ec55afd617b6d6fee33cfe7
Jacobsprint <112713231+Jacobsprint@users.noreply.github.com>
2022-09-13 01:52:19 +0000

    pdate

---
+++ karel28.py
@@ -0,0 +1,60 @@
+from stanfordkarel import *
+
+
+def main():
+    move()
+    move()
+    move()
+    move()
+    turn_left()
+    move()
+    pick_beeper()
+    move()
+    pick_beeper()
+    move()
+    pick_beeper()
+    move()
+    pick_beeper()
+    move()
+    pick_beeper()
+    turn_left()
+    move()
+    move()
+    turn_left()
+    move()
+    move()
+    put_beeper()
+    turn_left()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    move()
+    put_beeper()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+      
+    
+    
+    
+    
+    
+    
+    
+    
+      
+    
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
---
+++ karel29.py
@@ -0,0 +1,52 @@
+from stanfordkarel import *
+
+
+def main():
+  move()
+  move()
+  move()
+  turn_left()
+  move()
+  move()
+  turn_left()
+  turn_left()
+  turn_left()
+  move()
+  move()
+  turn_left()
+  move()
+  move()
+  turn_left()
+  move()
+  move()
+  turn_left()
+  turn_left()
+  turn_left()
+  move()
+  move()
+  turn_left()
+  turn_left()
+  turn_left()
+  move()
+  move()
+  turn_left()
+  turn_left()
+  turn_left()
+  move()
+  move()
+  turn_left()
+  turn_left()
+  turn_left()
+  move()
+  move()
+  turn_left()
+  move()
+  move()
+  turn_left()
+  
+  
+  pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
