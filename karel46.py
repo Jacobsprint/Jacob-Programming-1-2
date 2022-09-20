738ac6fc29101f5766b70004f888bc589bf03ed8
Jacobsprint <112713231+Jacobsprint@users.noreply.github.com>
2022-09-20 12:07:49 +0000

    Update

---
+++ karel46.py
@@ -0,0 +1,167 @@
+from stanfordkarel import *
+
+class ktools:
+  def m(self):
+    """Shorthand for move"""
+    move()
+  def tl(self):
+    """Turn Left"""
+    turn_left()
+  def tr(self):
+    """Turn Right"""
+    self.tl()
+    self.tl()
+    self.tl()
+  def ta(self):
+    """Turn Around"""
+    self.tl()
+    self.tl()
+  def pick(self):
+    """Pick Beeper"""
+    pick_beeper()
+  def put(self):
+    """Put Beeper"""
+    put_beeper()
+  def put2(self):
+    """Put two beepers in row"""
+    self.put()
+    self.m()
+    self.put()
+  def put5(self):
+    """Put five beepers in a row"""
+    self.put2()
+    self.m()
+    self.put2()
+    self.m()
+    self.put()
+  def h(self):
+    """Print the letter H using beepers"""
+    self.tl()
+    self.put5()
+    self.tr()
+    self.m()
+    self.m()
+    self.m()
+    self.tr()
+    self.put5()
+    self.ta()
+    self.m()
+    self.m()
+    self.tl()
+    self.m()
+    self.put2()
+    self.tl()
+    self.m()
+    self.m()
+    self.tl()
+    self.m()
+    self.m()
+    self.m()
+    self.m()
+
+  def e(self):
+    """Print letter E in beepers"""
+    self.tl()
+    self.put5()
+    self.tr()
+    self.m()
+    self.put2()
+    self.tr()
+    self.m()
+    self.m()
+    self.tr()
+    self.put2()
+    self.tl()
+    self.m()
+    self.m()
+    self.tl()
+    self.put2()
+    self.m()
+    self.m()
+  def l(self):
+    """Print letter L in beepers"""
+    self.tl()
+    self.put5()
+    self.ta()
+    self.m()
+    self.m()
+    self.m()
+    self.m()
+    self.tl()
+    self.m()
+    self.put2()
+    self.m()
+    self.m()
+
+  def o(self):
+    self.tl()
+    self.put5()
+    self.tr()
+    self.m()
+    self.put2()
+    self.m()
+    self.tr()
+    self.put5()
+    self.tr()
+    self.m()
+    self.put2()
+    self.ta()
+    self.m()
+    self.m()
+    self.m()
+    
+def main():
+    kt=ktools()
+    kt.tl()
+    kt.m()
+    kt.m()
+    kt.tr()
+    kt.m()
+    kt.tr()
+    kt.m()
+    kt.m()
+    kt.ta()
+    kt.m()
+    kt.tr()
+    kt.m()
+    kt.tr()
+    kt.m()
+    kt.tl()
+    kt.m()
+    kt.tl()
+    kt.m()
+    kt.m()
+    kt.m()
+    kt.tr()
+    kt.m()
+    kt.tr()
+    kt.m()
+    kt.m()
+    kt.m()
+    kt.tl()
+    kt.m()
+    kt.tl()
+    kt.m()
+    kt.m()
+    kt.tr()
+    kt.m()
+    kt.tr()
+    kt.m()
+    kt.m()
+    kt.tl()
+    kt.m()
+    kt.tl()
+    kt.m()
+    kt.m()
+    kt.tr()
+    kt.m()
+    kt.tr()
+    kt.m()
+    kt.m()
+    kt.tl()
+
+    pass
+
+
+if __name__ == "__main__":
+    run_karel_program()
