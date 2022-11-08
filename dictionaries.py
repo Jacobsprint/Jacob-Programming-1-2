0cb3336f95c155f74f3a95c13af55294b71391d0
Jacobsprint <112713231+Jacobsprint@users.noreply.github.com>
2022-11-08 15:13:58 +0000

    Update

---
+++ dictionaries.py
@@ -0,0 +1,10 @@
+dctnry = {}
+dctnry2 = {1:"apples", 2: "oranges", 3: "pears"}
+dctnry3 = {"name": "John", "clr": "Red", 0: [1,2,3]}
+
+print(dctnry2[2])
+print(dctnry3["name"])
+print(dctnry2)
+print(dctnry3)
+print(dctnry2.keys())
+print(dctnry2.values())
