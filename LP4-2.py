3f8b88f34788ac2922c93be940a3738d770f311a
Jacobsprint <112713231+Jacobsprint@users.noreply.github.com>
2022-11-21 15:38:01 +0000

    Update

---
+++ LP4-2.py
@@ -0,0 +1,17 @@
+def main():
+  weight = int(input(" Enter weight in kilograms:"))
+  length = int(input(" Enter length in centimeters:"))
+  width = int(input(" Enter width in centimeters:"))
+  height = int(input(" Enter height in centimeters:"))
+  cube = 0.0
+  
+  if weight >= 27:
+    return False
+  cube = length * width * height
+  
+  
+  pass
+
+
+if__name__ == "__main__":
+  main()
