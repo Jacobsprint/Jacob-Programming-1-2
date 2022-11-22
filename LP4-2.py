aeaf3a1d6b50eac03b3832ae6fdb765716023c6e
Jacobsprint <112713231+Jacobsprint@users.noreply.github.com>
2022-11-22 15:24:00 +0000

    Update

--- LP4-2.py
+++ LP4-2.py
@@ -6,12 +6,19 @@ def main():
   cube = 0.0
   
   if weight >= 27:
-    return False
+    print("Too Heavy")
+  else:
+    return True
   cube = length * width * height
-  
-  
+  if cube >= 100000:
+    print("Too Large")
+  else: 
+    return True
+  if cube >= 100000 and weight >= 27:
+    print("Too Heavy and Too Large")
+    
   pass
 
 
-if__name__ == "__main__":
+if __name__ == "__main__":
   main()
